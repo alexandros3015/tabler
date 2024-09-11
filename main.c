d3a79be1c34713cdb0ca4eeb4193110ff03f039f
alexandros3015 <105521378+alexandros3015@users.noreply.github.com>
2024-09-10 22:55:03 -0400

    semicolons

--- main.c
+++ main.c
@@ -132,11 +132,11 @@ int main(int argc, char *argv[]) {
     }
     
     if (arorgeo == 0) {
-        printf("Equation: f(n) = %lf + %lf * n", intercept, rate)
+        printf("Equation: f(n) = %lf + %lf * n", intercept, rate);
     } else if (arorgeo == 1) {
-        printf("Equation: f(n) = %lf * %lf ^ n", intercept, rate)
+        printf("Equation: f(n) = %lf * %lf ^ n", intercept, rate);
     } else if (arorgeo == 2) {
-        printf("Equation: f(n) = %lf * e ^ (%lf * n)", intercept, rate)
+        printf("Equation: f(n) = %lf * e ^ (%lf * n)", intercept, rate);
     }    
 
     fclose(fp);
