b4054e2f1184ad7eea6d0d13adc36d781d13754d
alexandros3015 <alexandrosbeachtoys@gmail.com>
2025-11-13 21:35:26 -0500

    Add --help option to print usage information
    Introduces a --help command line option to display usage instructions and available options for the program. This improves user experience by providing guidance on how to use the tool.

--- main.c
+++ main.c
@@ -36,16 +36,32 @@ int main(int argc, char *argv[]) {
     int edge = 0;
     if (argc > 1) {
         for (int x = 0; x < argc; x++) {
+            
             if (strcmp(argv[x], "--p") == 0 || strcmp(argv[x], "--plot") == 0) {
                 plotornah = 1;
             }
+            
             else if (strcmp(argv[x], "--noprint") == 0 || strcmp(argv[x], "--n") == 0) {
                 printornah = 0;
-            } else if (strcmp(argv[x], "--remove") == 0) {
+            } 
+            
+            else if (strcmp(argv[x], "--remove") == 0) {
                 deleteafter = 0;
             } 
+            
             else if (strcmp(argv[x], "--edge") == 0) {
             	edge = 1;
+            }
+
+            else if (strcmp(argv[x], "--help") == 0) {
+                printf("Usage: tabler [options]\n");
+                printf("Options:\n");
+                printf("  --p, --plot: Plot the graph\n");
+                printf("  --noprint, --n: Don't print the graph\n");
+                printf("  --remove: Remove the file after plotting\n");
+                printf("  --edge: Haha funny edge (suggested by dalepwo)\n");
+                printf("  --help: Print this help message\n");
+                return 0;
             }
         }
     }
